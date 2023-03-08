e40226c58fdeb54750746a88ffcf8422c5d9c886
aissata <aimaiga2@gmail.com>
2023-03-08 17:19:50 +0100

    removed an infinite loop

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use log::info;
 use regex::Regex;
 use std::collections::HashMap;
 use std::fs::{self, File};
@@ -7,15 +8,24 @@ use std::io::BufReader;
 use std::path::{Path, PathBuf};
 
 fn main() -> Result<()> {
+    env_logger::init();
+    info!("Starting application");
     //let current_dir: PathBuf = env::current_dir()?;
-    let path = Path::new("../syn/src");
+    let path = Path::new("../famous_crates");
     let number_regex = Regex::new(r#"\d+"#).unwrap();
     let struct_regex = Regex::new(r"struct").unwrap();
     let enum_regex = Regex::new(r"enum").unwrap();
     let array_regex = Regex::new(r"\[[^;\]]+;\s*\d+\]").unwrap();
-
-    let dyn_vec = Regex::new(r"(vec!\[\])|(Vec<)|Vec::n").unwrap();
-    let regexes = vec![number_regex, struct_regex, enum_regex, array_regex, dyn_vec];
+    let dyn_vec_regex = Regex::new(r"(vec!\[\])|(Vec<)|Vec::n").unwrap();
+    let string_regex = Regex::new(r"String|to_string|format!").unwrap();
+    let regexes = vec![
+        number_regex,
+        struct_regex,
+        enum_regex,
+        array_regex,
+        dyn_vec_regex,
+        string_regex,
+    ];
 
     let mut results: HashMap<String, i32> = HashMap::new();
     let mut total_line_counter = 0;
@@ -43,22 +53,31 @@ fn process_directory(
 ) -> Result<(), anyhow::Error> {
     for entry in fs::read_dir(path)? {
         let entry = entry?;
+        //println!("entry file name {:?}", entry.path().to_string_lossy());
+
         let path = entry.path();
 
-        for r in regexes {
-            if path.is_file() {
-                *total_number_of_files += 1;
+        println!("Path file name {:?}", path.to_string_lossy());
+        //let is_src_or_test =
+        //    path.to_string_lossy().contains("src/") || path.to_string_lossy().contains("tests/");
+        //let is_src = path.file_name().map_or(false, |name| name == "src/");
+
+        //  && is_src_or_test
+        if path.is_file() {
+            *total_number_of_files += 1;
+            info!("File treated: {:?}", path.as_path());
+            for r in regexes {
                 count_in_file(&path, r, results, total_line_counter);
-            } else if path.is_dir() {
-                println!("Changing dir to {path:?}");
-                process_directory(
-                    &path,
-                    regexes,
-                    results,
-                    total_line_counter,
-                    total_number_of_files,
-                )?;
             }
+        } else if path.is_dir() {
+            info!("Changing dir to {path:?}");
+            process_directory(
+                &path,
+                regexes,
+                results,
+                total_line_counter,
+                total_number_of_files,
+            )?;
         }
     }
     Ok(())
