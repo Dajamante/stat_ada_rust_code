510f7c87a90d8a7a461e1bbe443a3d7174dec172
aissata <aimaiga2@gmail.com>
2023-03-13 17:00:07 +0100

    change for effective iteration

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,17 @@ use std::path::{Path, PathBuf};
 fn main() -> Result<()> {
     env_logger::init();
     info!("Starting application");
-    //let current_dir: PathBuf = env::current_dir()?;
-    let path = Path::new("../famous_crates");
-    let number_regex = Regex::new(r#"\d+"#).unwrap();
+
+    let number_regex = Regex::new(r#"\b\d+\b"#).unwrap();
     let struct_regex = Regex::new(r"struct").unwrap();
     let enum_regex = Regex::new(r"enum").unwrap();
-    let array_regex = Regex::new(r"\[[^;\]]+;\s*\d+\]").unwrap();
+    let array_regex = Regex::new(r"\[.*;.*\]").unwrap();
+    let ref_regex = Regex::new(r"&[mut\s]*\w+").unwrap();
+    let heap_regex: Regex = Regex::new(r"Box<[^<>]+>|Rc<[^<>]+>|Arc<[^<>]+>").unwrap();
+    //let unsafe_regex: Regex = Regex::new(r"unsafe \{").unwrap();
+
     let dyn_vec_regex = Regex::new(r"(vec!\[\])|(Vec<)|Vec::n").unwrap();
-    let string_regex = Regex::new(r"String|to_string|format!").unwrap();
+    let string_regex = Regex::new(r"String::|.to_string|format!\(").unwrap();
     let regexes = vec![
         number_regex,
         struct_regex,
@@ -25,21 +28,67 @@ fn main() -> Result<()> {
         array_regex,
         dyn_vec_regex,
         string_regex,
+        heap_regex,
+        ref_regex,
     ];
 
-    let mut results: HashMap<String, i32> = HashMap::new();
-    let mut total_line_counter = 0;
-    let mut total_number_of_files = 0;
-    process_directory(
-        path,
-        &regexes,
-        &mut results,
-        &mut total_line_counter,
-        &mut total_number_of_files,
-    )?;
-    println!("Hashmap : {results:?}");
-    println!("Total lines : {total_line_counter}");
-    println!("Total number of files : {total_number_of_files}");
+    let path = Path::new("../famous_crates");
+
+    let mut all_hashmaps_vec: Vec<HashMap<String, i32>> = Vec::new();
+
+    for sub_dir in path.read_dir().expect("Could not access the famous dir") {
+        let mut results: HashMap<String, i32> = HashMap::new();
+        let mut total_line_counter = 0;
+        let mut total_number_of_files = 0;
+        let sub_dir = sub_dir.expect("Could not read subdirectory");
+        let sub_dir_path = sub_dir.path();
+
+        // For now let's avoid Ada
+        if sub_dir_path.to_string_lossy().contains("ada") {
+            continue;
+        }
+        process_directory(
+            &sub_dir_path,
+            &regexes,
+            &mut results,
+            &mut total_line_counter,
+            &mut total_number_of_files,
+        )
+        .unwrap_or_else(|err| {
+            eprintln!(
+                "Error processing directory {}: {}",
+                sub_dir_path.display(),
+                err
+            );
+        });
+
+        println!("Dir : {:#?} : {results:#?}", &sub_dir_path.display());
+
+        let total: i32 = results.values().sum();
+        println!("Number of total occurences : {total}");
+        for (k, v) in &results {
+            println!(
+                "key : {k}, value : {v}, percentage {:.4}",
+                (*v as f64 / total as f64)
+            );
+        }
+        println!("Total LOC : {total_line_counter}");
+        println!("Total number of files : {total_number_of_files}\n\n");
+
+        all_hashmaps_vec.push(results);
+    }
+
+    let mut final_hashmap = HashMap::new();
+    for hm in all_hashmaps_vec {
+        for (k, v) in hm {
+            match final_hashmap.get(&k) {
+                Some(count) => final_hashmap.insert(k, v + count),
+                None => final_hashmap.insert(k, v),
+            };
+        }
+    }
+
+    println!("Final result: {final_hashmap:?}");
 
     Ok(())
 }
@@ -53,24 +102,15 @@ fn process_directory(
 ) -> Result<(), anyhow::Error> {
     for entry in fs::read_dir(path)? {
         let entry = entry?;
-        //println!("entry file name {:?}", entry.path().to_string_lossy());
 
         let path = entry.path();
 
-        println!("Path file name {:?}", path.to_string_lossy());
-        //let is_src_or_test =
-        //    path.to_string_lossy().contains("src/") || path.to_string_lossy().contains("tests/");
-        //let is_src = path.file_name().map_or(false, |name| name == "src/");
-
-        //  && is_src_or_test
         if path.is_file() {
             *total_number_of_files += 1;
-            info!("File treated: {:?}", path.as_path());
             for r in regexes {
                 count_in_file(&path, r, results, total_line_counter);
             }
         } else if path.is_dir() {
-            info!("Changing dir to {path:?}");
             process_directory(
                 &path,
                 regexes,
@@ -94,11 +134,27 @@ fn count_in_file(
     reader.lines().flatten().for_each(|line| {
         *total_line_counter += 1;
         if regex.is_match(&line) {
-            let key = regex.as_str().to_string();
+            let key = create_key(regex);
             match results.get(&key) {
                 Some(count) => results.insert(key, count + 1),
                 None => results.insert(key, 1),
             };
         }
     });
+}
+
+fn create_key(regex: &Regex) -> String {
+    let key = match regex.as_str() {
+        "\\[.*;.*\\]" => "Array".to_string(),
+        "Box<[^<>]+>|Rc<[^<>]+>|Arc<[^<>]+>" => "Box/Rc/Arc".to_string(),
+        "&[mut\\s]*\\w+" => "&T or &mut T".to_string(),
+        "String::|.to_string|format!\\(" => "String".to_string(),
+        "(vec!\\[\\])|(Vec<)|Vec::n" => "Vec".to_string(),
+        "\\b\\d+\\b" => "Number".to_string(),
+        "struct" => "Struct".to_string(),
+        "enum" => "Enum".to_string(),
+        // this is a kind of error I guess
+        _ => String::from("Not a regex"),
+    };
+    key
 }
